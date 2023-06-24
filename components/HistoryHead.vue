ead622c6130d6fa0651adc14038fd79c56a9d99c
Rwepka <rwepka@gmail.com>
2023-06-24 14:19:58 +0300

    Пятая страница готова

---
+++ components/HistoryHead.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="history__head">
+    <div class="head__head">ИСТОРИЯ ПОКУПОК</div>
+    <div class="head__body">
+      <div class="body__last">Последние покупки</div>
+      <div>Январь</div>
+      <div>Февраль</div>
+      <div>Март</div>
+      <div>Апрель</div>
+      <div>Май</div>
+      <div>Июнь</div>
+      <div>Июль</div>
+      <div>Август</div>
+      <div>Сентябрь</div>
+      <div><img src="/assets/arrow__bot.svg" alt="" class="head__arrow"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.history__head{
+  margin-left: 375px;
+  margin-top: 60px;
+  margin-bottom: 31px;
+}
+.head__head {
+  font-size: 48px;
+  color: #545155
+}
+.head__body {
+  display: flex;
+  gap: 47px;
+  margin-top: 31px;
+  margin-bottom: 50px;
+  color: #112E4C;
+}
+.head__arrow {
+  transform: rotate(270deg);
+}
+.body__last {
+  color: #BF1E77
+}
+</style>
--- pages/history.vue
+++ pages/history.vue
@@ -1,13 +1,32 @@
 <template>
   <title>История</title>
+  <HistoryHead />
+  <div class="products__list">
+    <StoreOfCards 
+    v-for="card of cardStore.cards"
+    :key="card.id"
+    :card="card"/>
+  </div>
+  <PageList />
 </template>
 
 <script setup>
 definePageMeta({
   layout: 'other'
 })
+import PageList from '~/components/HistoryPageList.vue';
+import HistoryHead from '~/components/HistoryHead.vue';
+import StoreOfCards from '/components/HistoryStoreOfCards.vue'
+import { useCardStore } from '/stores/ProductCards.js'
+const cardStore = useCardStore()
 </script>
 
-<style>
-
+<style scoped>
+.products__list {
+  display: grid;
+  grid-template-columns: 270px 270px 270px 270px;
+  gap: 30px;
+  row-gap: 50px;
+  margin-left: 375px;
+}
 </style>
