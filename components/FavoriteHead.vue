dbbf450a6171d1b8137ad9837d8557440460113a
Rwepka <rwepka@gmail.com>
2023-06-24 14:34:06 +0300

    Шестая страница готова

---
+++ components/FavoriteHead.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="favorite__head">
+    <div class="head__head">ИЗБРАННЫЕ</div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.favorite__head{
+  margin-left: 375px;
+  margin-top: 60px;
+  margin-bottom: 31px;
+}
+.head__head {
+  font-size: 48px;
+  color: #545155
+}
+</style>
--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,13 +1,31 @@
 <template>
   <title>Избранные</title>
+  <FavoriteHead />
+  <div class="products__list">
+    <StoreOfCards 
+    v-for="card of cardStore.cards"
+    :key="card.id"
+    :card="card"/>
+  </div>
+  <FavoritePageList />
 </template>
 
 <script setup>
 definePageMeta({
   layout: 'other'
 })
+import FavoriteHead from '/components/FavoriteHead.vue'
+import StoreOfCards from '/components/FavoriteStoreOfCards.vue'
+import { useCardStore } from '/stores/ProductCards.js'
+import FavoritePageList from '/components/FavoritePageList.vue'
+const cardStore = useCardStore()
 </script>
-
 <style>
-
+.products__list {
+  display: grid;
+  grid-template-columns: 270px 270px 270px 270px;
+  gap: 30px;
+  row-gap: 50px;
+  margin-left: 375px;
+}
 </style>
